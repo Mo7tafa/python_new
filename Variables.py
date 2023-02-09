266e3187457d86efea08ce12c2d61062d72b915b
Hades2460 <124900574+Hades2460@users.noreply.github.com>
2023-02-09 20:28:22 +0330

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,67 @@
+# phrase = "Parham the best"
+# phrase2 = phrase + " is nice"
+# phrase3 = phrase.lower()
+# phrase4 = phrase.replace("a" , "A")
+# phrase5 = phrase.upper()
+
+# print(phrase2)
+# print(len(phrase))
+# print(phrase.index("be"))
+# print(phrase[8])
+
+# num1 = 10
+# num2 = 3
+# result = num1 % num2
+# print(result)
+#                                                 abs command (Unknown)
+# num1 = 10
+# num2 = -  3
+# result = num1 % num2
+# print(abs(result))
+#                                                 making a sentence with input
+# name = input("please insert your name: ")
+# age = input("please enter your age: ")
+
+# print("Your name is " + name + "\nAnd you are " + age + " years old. ")
+
+
+#                                                 Creating a calculator
+# num1 = input("Please insert your first number: ")
+# num2 = input("Please insert your secound number: ")
+# operation = input("Please choose your desired operation: +, *, /,- ")
+
+# if operation == "+":
+#     result = float(num1) + float(num2)
+# if operation == "-":
+#     result = float(num1) - float(num2)
+# if operation == "*":
+#     result = float(num1) * float(num2)
+# if operation == "/":
+#     result = float(num1) / float(num2)
+# else:
+#     print("The operation is not / ")
+
+# print(result)
+
+#                                             Going to next operation ONLY if the previous one is not valid
+
+# if operation == "+":
+#     result = float(num1) + float(num2)
+# elif operation == "-":
+#     result = float(num1) - float(num2)
+# elif operation == "*":
+#     result = float(num1) * float(num2)
+# elif operation == "/":
+#     result = float(num1) / float(num2)
+# else:
+#     result = ("the operation inserted by the user is not valid! ")
+
+# print(result)
+
+#                                             Practise List
+names = ["ali", "reza", True]
+
+def chosen_names(names):
+    print(len(names))
+print(chosen_names)
+
